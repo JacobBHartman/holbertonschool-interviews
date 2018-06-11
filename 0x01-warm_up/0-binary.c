76142e942ab169dc70b4bf5b64e132689e6dad62
JacobBHartman <jacobbhartman@protonmail.com>
2018-06-11 23:08:42 +0000

    initial commit for task

--- 0x01-warm_up/0-binary.c
+++ 0x01-warm_up/0-binary.c
@@ -35,7 +35,7 @@ int binary_search(int *array, size_t size, int value)
     if (array[midpoint] == value)
       return (midpoint);
     else if (array[midpoint] > value)
-      high = midpoint;
+      high = midpoint + 1;
     else if (array[midpoint] < value)
       low = midpoint + 1;
     midpoint = (high + low) / 2;
